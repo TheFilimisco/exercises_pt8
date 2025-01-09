c00a84638d238f47713f77ddd14d34836b4cfdcc
Steven Uriarte <54910978l@iespoblenou.org>
2025-01-09 14:30:38 +0100

    first exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Put your length's arrays: ");
+        int N = input.nextInt();
+        int[] array = new int[N];
+        int acum = 10;
+        for(int i=0; i<array.length; i++) {
+            array[i]=acum;
+            acum+=10;
+            System.out.print(array[i] + " ");
+        }
+
+        int[] array_reverse = new int[N];
+        for (int i = 0; i < array.length; i++) {
+            int less = array[array.length-1];
+            array_reverse[i] = less;
+            less-=10;
+            System.out.println(array_reverse[i] + " ");
+        }
+
     }
 }
